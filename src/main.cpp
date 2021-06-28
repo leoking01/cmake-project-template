1383651ca52c17e17c5589fc9c197452496ba31b
Your Name <you@example.com>
2021-06-27 22:59:57 -0700

    sss.5566

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 static const char *const HEADER = "\nDivider © 2018 Monkey Claps Inc.\n\n";
 static const char *const USAGE = "Usage:\n\tdivider <numerator> <denominator>\n\nDescription:\n\tComputes the result of a fractional division,\n\tand reports both the result and the remainder.\n";
 
-int main(int argc, const char *argv[]) {
+int main_exe(int argc, const char *argv[]) {
   Fraction f;
 
   cout << HEADER;
@@ -30,4 +30,13 @@ int main(int argc, const char *argv[]) {
     cout << "Can not divide by zero, Homer. Sober up!\n";
   }
   return 0;
+}
+
+
+int main()
+{
+int argc= 3;
+const char *argv[] = {"aa",    "3",  "6"   }  ;
+main_exe( argc,  argv );
+    return  0 ;
 }
